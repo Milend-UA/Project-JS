2400e97898202ec6f5ec701010a65f92f95a6cbb
Milend-UA <vitaliymilend@gmail.com>
2024-09-07 21:01:48 +0300

    Commit 1

--- Project/script.js
+++ Project/script.js
@@ -2,15 +2,17 @@ fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => {
         let usersContainer = document.getElementById('users-container');
-        users.forEach(user => {
+
+        for (let i = 0; i < users.length; i++) {
+            let user = users[i];
+
             let userBlock = document.createElement('div');
             userBlock.className = 'user-block';
-
             userBlock.innerHTML = `
                 <p>ID: ${user.id}</p>
                 <p>Name: ${user.name}</p>
                 <a href="user-details.html?id=${user.id}">View Details</a>
             `;
             usersContainer.appendChild(userBlock);
-        });
+        }
     })
--- Project/user-details.js
+++ Project/user-details.js
@@ -1,3 +1,4 @@
+// Получаем параметры URL
 let urlParams = new URLSearchParams(window.location.search);
 let userId = urlParams.get('id');
 
@@ -6,23 +7,21 @@ fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
     .then(user => {
         let userDetails = document.getElementById('user-details');
         userDetails.innerHTML = `
-    <h2>User Details</h2>
-    <p><strong>ID:</strong> ${user.id}</p>
-    <p><strong>Name:</strong> ${user.name}</p>
-    <p><strong>Username:</strong> ${user.username}</p>
-    <p><strong>Email:</strong> ${user.email}</p>
-    <p><strong>Address:</strong> ${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</p>
-    <p><strong>Geo:</strong> Lat: ${user.address.geo.lat}, Lng: ${user.address.geo.lng}</p>
-    <p><strong>Phone:</strong> ${user.phone}</p>
-    <p><strong>Website:</strong> ${user.website}</p>
-    <p><strong>Company:</strong> ${user.company.name}</p>
-    <p><strong>Catchphrase:</strong> ${user.company.catchPhrase}</p>
-    <p><strong>Business:</strong> ${user.company.bs}</p>
-`;
-
+            <h2>User Details</h2>
+            <p><strong>ID:</strong> ${user.id}</p>
+            <p><strong>Name:</strong> ${user.name}</p>
+            <p><strong>Username:</strong> ${user.username}</p>
+            <p><strong>Email:</strong> ${user.email}</p>
+            <p><strong>Address:</strong> ${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</p>
+            <p><strong>Geo:</strong> Lat: ${user.address.geo.lat}, Lng: ${user.address.geo.lng}</p>
+            <p><strong>Phone:</strong> ${user.phone}</p>
+            <p><strong>Website:</strong> ${user.website}</p>
+            <p><strong>Company:</strong> ${user.company.name}</p>
+            <p><strong>Catchphrase:</strong> ${user.company.catchPhrase}</p>
+            <p><strong>Business:</strong> ${user.company.bs}</p>
+        `;
     })
 let loadPostsButton = document.getElementById('load-posts');
-
 loadPostsButton.addEventListener('click', () => {
     fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
         .then(response => response.json())
@@ -30,15 +29,17 @@ loadPostsButton.addEventListener('click', () => {
             let postsContainer = document.getElementById('posts-container');
             // postsContainer.innerHTML = '<h3>Posts</h3>';
 
-            posts.forEach(post => {
+            for (let i = 0; i < posts.length; i++) {
+                let post = posts[i];
+
                 let postBlock = document.createElement('div');
                 postBlock.className = 'post-block';
+
                 postBlock.innerHTML = `
                     <p><strong>Title:</strong> ${post.title}</p>
                     <a href="post-details.html?postId=${post.id}">View Post Details</a>
                 `;
                 postsContainer.appendChild(postBlock);
-            });
+            }
         })
 });
-
